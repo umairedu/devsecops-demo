59dc7f16feebe5887ff38b9cb3107071ae4f7dad
Umair Khan <umairkhan@MacBookPro.lan>
2025-11-23 12:23:06 +0500

    resolve test hanging issue

--- app/index.js
+++ app/index.js
@@ -34,9 +34,11 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
 module.exports = { hello, server };
 
